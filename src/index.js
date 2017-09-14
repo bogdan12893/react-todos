edff05f26f92f38f9d1ab975bb1556baed9475a4
Bogdan Cadar <bogdan1281993@gmail.com>
2017-09-14 22:21:39 +0300

    Todo app finish

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Todo} from './components/main';
+import registerServiceWorker from './registerServiceWorker';
+
+var tasksList = ["Task 1", "Task 2"]
+
+var tasks = localStorage.getItem('storedTasks');
+if(tasks){
+    tasksList = JSON.parse(tasks);
+}
+
+ReactDOM.render(
+    <Todo tasks={tasksList} />,
+    document.getElementById('root'));
+    registerServiceWorker();
